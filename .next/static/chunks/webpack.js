fa70d634dcb7b8880892e6aba26f78cb067f94da
Invest <56314267-smartinvest414@users.noreply.replit.com>
2026-03-21 08:48:09 +0000

    Error fix and admin page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b4076a050687f1e0"; }
+/******/ 		__webpack_require__.h = function() { return "5e276bf23440d8d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
